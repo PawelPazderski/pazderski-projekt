9b768f29efe46c9a14baf215bd4242d03e0184ef
PawelPazderski <pazderski.projekt@gmail.com>
2023-06-21 12:15:53 +0200

    style: contact header

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,8 +3,8 @@
 export default function Contact() {
 
   return (
-    <main className="mx-auto px-2.5 max-w-screen-lg py-10">
-      <h1 className="mb-3 text-lg">Kontakt</h1>
+    <main className="mx-auto px-2.5 max-w-screen-lg py-10 text-center">
+      <h1 className="mb-3 text-lg font-bold tracking-wider">Kontakt</h1>
     </main>
   );
 }
